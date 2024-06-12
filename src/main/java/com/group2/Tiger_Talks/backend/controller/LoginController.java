fd6c759487ac1f4ab1224515012c71e4c5d9549d
hongh931 <hn582183@dal.ca>
2024-06-12 09:37:43 -0300

    feat: log in service implementation

--- src/main/java/com/group2/Tiger_Talks/backend/controller/LoginController.java
+++ src/main/java/com/group2/Tiger_Talks/backend/controller/LoginController.java
@@ -1,4 +1,31 @@
 package com.group2.Tiger_Talks.backend.controller;
 
+import com.group2.Tiger_Talks.backend.model.User.UserTemplate;
+import com.group2.Tiger_Talks.backend.service.Authentication.LogInService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/logIn")
 public class LoginController {
+
+    @Autowired
+    private LogInService logInService;
+
+    @PostMapping("/userLogIn")
+    public ResponseEntity<String> logIn(@RequestParam String email, @RequestParam String password) {
+        Optional<UserTemplate> result = logInService.logInUserTemplate(email, password);
+        if (result.isPresent()) {
+            return ResponseEntity.ok("Welcome, " + result.get().getEmail());
+        } else {
+            return ResponseEntity.status(401).body("invalid email or password.");
+        }
+    }
+
 }
--- src/main/java/com/group2/Tiger_Talks/backend/service/implementation/Authentication/LogInServiceImpl.java
+++ src/main/java/com/group2/Tiger_Talks/backend/service/implementation/Authentication/LogInServiceImpl.java
@@ -1,6 +1,34 @@
 package com.group2.Tiger_Talks.backend.service.implementation.Authentication;
 
+import com.group2.Tiger_Talks.backend.model.User.UserTemplate;
+import com.group2.Tiger_Talks.backend.repsitory.User.UserTemplateRepository;
 import com.group2.Tiger_Talks.backend.service.Authentication.LogInService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Service
 public class LogInServiceImpl implements LogInService {
+
+    @Autowired
+    private UserTemplateRepository userTemplateRepository;
+
+    @Override
+    public Optional<UserTemplate> logInUserTemplate(String email, String password) {
+        Optional<UserTemplate> userTemp = userTemplateRepository.findUserTemplateByEmail(email);
+        if (userTemp.isPresent()) {
+            UserTemplate user = userTemp.get();
+            if (user.getPassword().equals(password)) {
+                return Optional.of(user);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public Optional<UserTemplate> getUserByEmail(String email) {
+        return userTemplateRepository.findUserTemplateByEmail(email);
+    }
+
 }
