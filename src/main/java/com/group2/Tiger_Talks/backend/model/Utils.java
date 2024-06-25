5a4c9815c8f2b174833c41e251c65688e659b3af
Osebhulimen Egbor <os614875@dal.ca>
2024-06-25 13:59:42 -0300

    docs: Added max & min values for B00 numbers

--- src/main/java/com/group2/Tiger_Talks/backend/model/Utils.java
+++ src/main/java/com/group2/Tiger_Talks/backend/model/Utils.java
@@ -2,6 +2,9 @@ package com.group2.Tiger_Talks.backend.model;
 
 public final class Utils {
     public static final String CROSS_ORIGIN_HOST_NAME = "http://localhost:3000";
+    public static final int BOO_MAX = 999999;
+    public static final int BOO_MIN = 0;
+
 
     public static final class UserLevel {
         public static final String ADMIN = "admin";
