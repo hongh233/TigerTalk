1764ed4ad21cec721602345092331440b83385fe
Osebhulimen Egbor <os614875@dal.ca>
2024-06-25 13:58:38 -0300

    docs: Extracted passwordReset into the request mappping

--- src/main/java/com/group2/Tiger_Talks/backend/controller/PasswordResetController.java
+++ src/main/java/com/group2/Tiger_Talks/backend/controller/PasswordResetController.java
@@ -9,14 +9,14 @@ import org.springframework.web.bind.annotation.*;
 import static com.group2.Tiger_Talks.backend.model.Utils.CROSS_ORIGIN_HOST_NAME;
 
 @RestController
-@RequestMapping("/api/logIn")
+@RequestMapping("/api/passwordReset")
 public class PasswordResetController {
 
     @Autowired
     private PasswordResetService passwordResetService;
 
     @CrossOrigin(origins = CROSS_ORIGIN_HOST_NAME)
-    @PostMapping("passwordReset/validateEmailExist")
+    @PostMapping("/validateEmailExist")
     public ResponseEntity<String> validateEmailExist(@RequestParam("email") String email) {
         return passwordResetService.validateEmailExist(email)
                 .map(err -> ResponseEntity.status(400).body(err))
@@ -24,7 +24,7 @@ public class PasswordResetController {
     }
 
     @CrossOrigin(origins = CROSS_ORIGIN_HOST_NAME)
-    @PostMapping("passwordReset/resetPassword")
+    @PostMapping("/resetPassword")
     public ResponseEntity<String> resetPassword(@RequestBody ForgotPasswordDTO passwordDTO) {
         return passwordResetService.resetPassword(passwordDTO)
                 .map(err -> ResponseEntity.status(404).body(err))
@@ -32,7 +32,7 @@ public class PasswordResetController {
     }
 
     @CrossOrigin(origins = CROSS_ORIGIN_HOST_NAME)
-    @PostMapping("passwordReset/sendToken")
+    @PostMapping("/sendToken")
     public ResponseEntity<String> sendToken(@RequestParam("email") String email) {
         return passwordResetService.createAndSendResetMail(email)
                 .map(err -> ResponseEntity.status(400).body(err))
@@ -40,7 +40,7 @@ public class PasswordResetController {
     }
 
     @CrossOrigin(origins = CROSS_ORIGIN_HOST_NAME)
-    @PostMapping("passwordReset/checkToken/{token}")
+    @PostMapping("/checkToken/{token}")
     public ResponseEntity<String> validateToken(@PathVariable("token") String token) {
         return passwordResetService.validateToken(token)
                 .map(err -> ResponseEntity.status(404).body(err))
@@ -48,7 +48,7 @@ public class PasswordResetController {
     }
 
     @CrossOrigin(origins = CROSS_ORIGIN_HOST_NAME)
-    @PostMapping("passwordReset/verifySecurityAnswers")
+    @PostMapping("/verifySecurityAnswers")
     public ResponseEntity<String> verifySecurityAnswers(
             @RequestParam("email") String email,
             @RequestParam("answer1") String answer1,
@@ -60,7 +60,7 @@ public class PasswordResetController {
     }
 
     @CrossOrigin(origins = CROSS_ORIGIN_HOST_NAME)
-    @GetMapping("passwordReset/getSecurityQuestions")
+    @GetMapping("/getSecurityQuestions")
     public ResponseEntity<String[]> getSecurityQuestions(@RequestParam("email") String email) {
         return ResponseEntity.ok(passwordResetService.getSecurityQuestions(email));
     }
