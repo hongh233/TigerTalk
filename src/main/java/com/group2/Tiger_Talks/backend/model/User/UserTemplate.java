20dc1e7e5c2d694ca986c80952a4f847b37bfbe2
Osebhulimen Egbor <os614875@dal.ca>
2024-06-25 18:00:47 -0300

    docs: Delete UserName,age,gender,fName&lName from user template

--- src/main/java/com/group2/Tiger_Talks/backend/model/User/UserTemplate.java
+++ src/main/java/com/group2/Tiger_Talks/backend/model/User/UserTemplate.java
@@ -12,17 +12,11 @@ public class UserTemplate {
     @Id
     private String email;
 
-    @Column(name = "user_name", unique = true)
-    private String userName;
+
     @Column(unique = true)
-    private int bannerID;    // B00XXXXXX (X represents integer from 0 to 9)
     private String password;
     private String userLevel = UserLevel.USER;   // admin / user
     private String status = UserStatus.PENDING;      // blocked / pending / active
-    private int age;
-    private String gender;
-    private String firstName;
-    private String lastName;
     private boolean isValidated = false;
     private String[] securityQuestions;
     private String[] securityQuestionsAnswer;
@@ -33,22 +27,10 @@ public class UserTemplate {
     @OneToOne(mappedBy = "userTemplate", cascade = CascadeType.ALL, orphanRemoval = true)
     private UserProfile userProfile = null;
 
-    public UserTemplate(String firstName,
-                        String lastName,
-                        Integer age,
-                        String gender,
-                        String userName,
-                        int bannerID,
-                        String email,
+    public UserTemplate(String email,
                         String password,
                         String[] securityQuestionsAnswer,
                         String[] securityQuestions) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.gender = gender;
-        this.userName = userName;
-        this.bannerID = bannerID;
         this.email = email;
         this.password = password;
         this.securityQuestionsAnswer = securityQuestionsAnswer;
@@ -58,54 +40,13 @@ public class UserTemplate {
     public UserTemplate() {
     }
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     private String toString_() {
         return "user_template{" +
-                ",\n bannerID='" + bannerID + '\'' +
                 ",\n email='" + email + '\'' +
                 ",\n password='" + password + '\'' +
                 ",\n userLevel='" + userLevel + '\'' +
                 ",\n status='" + status + '\'' +
                 "\n}\n";
-    }
-
-    public int getBannerID() {
-        return bannerID;
-    }
-
-    public void setBannerID(int BannerID) {
-        this.bannerID = BannerID;
     }
 
     public String getEmail() {
@@ -146,14 +87,6 @@ public class UserTemplate {
 
     public void setUserProfile(UserProfile userProfile) {
         this.userProfile = userProfile;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
     }
 
     public boolean isValidated() {
