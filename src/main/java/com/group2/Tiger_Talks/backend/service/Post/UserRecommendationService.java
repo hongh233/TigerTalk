9a252e2a80cd52ff9f19f984530a265ecb36b1f8
shuqiang <sh673592@dal.ca>
2024-06-26 18:55:41 -0300

    temp

--- src/main/java/com/group2/Tiger_Talks/backend/service/Post/UserRecommendationService.java
+++ src/main/java/com/group2/Tiger_Talks/backend/service/Post/UserRecommendationService.java
@@ -1,21 +1,22 @@
 package com.group2.Tiger_Talks.backend.service.Post;
 
 import com.group2.Tiger_Talks.backend.model.Socials.Friendship;
-import com.group2.Tiger_Talks.backend.repsitory.Socials.FriendshipRepository;
+import com.group2.Tiger_Talks.backend.model.UserProfile;
+import com.group2.Tiger_Talks.backend.repository.Socials.FriendshipRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
+/*
 @Service
 public class UserRecommendationService {
     @Autowired
     private FriendshipRepository friendshipRepository;
 
     public List<String> recommendFriends(String userEmail) {
-        List<Friendship> friendships = friendshipRepository.findBySendersEmailOrReceiversEmail(userEmail, userEmail);
+        List<Friendship> friendships = friendshipRepository.findBySenderOrReceiver(UserProfile sender, UserProfile receiver);
 
         List<String> friendsEmails = friendships.stream()
                 .flatMap(f -> Stream.of(f.getSendersEmail(), f.getReceiversEmail()))
@@ -26,4 +27,8 @@ public class UserRecommendationService {
         // This is a simple example. In real life, you would have more complex logic here.
         return friendsEmails.stream().limit(5).collect(Collectors.toList());
     }
+
+
 }
+
+ */
