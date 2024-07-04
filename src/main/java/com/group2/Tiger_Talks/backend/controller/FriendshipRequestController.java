1d0afc5976458602547c24b794f7e92c55133655
Osebhulimen Egbor <os614875@dal.ca>
2024-07-04 17:53:21 -0300

    feat: Abstracted creation of friends for readability

--- src/main/java/com/group2/Tiger_Talks/backend/controller/FriendshipRequestController.java
+++ src/main/java/com/group2/Tiger_Talks/backend/controller/FriendshipRequestController.java
@@ -7,13 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
-
-import static com.group2.Tiger_Talks.backend.model.Utils.TestGen.MAX_USERS;
-import static com.group2.Tiger_Talks.backend.model.Utils.TestGen.MIN_USERS;
 
 /**
  * REST controller for handling friendship requests.
@@ -79,69 +73,8 @@ public class FriendshipRequestController {
 
     @PostMapping("/Test")
     public String bulkFriends(@RequestParam("numOfUsers") int numOfUsers) {
-        if (numOfUsers > MAX_USERS || MIN_USERS > numOfUsers)
-            throw new IllegalArgumentException("Cannot generate more than 26 and less than 3 users as the names are A-Z check impl to change this behaviour");
-
-        int MAIN_USER = Test.alpha((numOfUsers / 2)) ^ 32;
-        Queue<Integer> queue = new LinkedList<>(List.of(MAIN_USER));
-        HashSet<Integer> friends = new HashSet<>();
-
-        final String email = "%c@dal.ca";
-
-        while (!queue.isEmpty()) {
-            int curr = queue.remove();
-            if (friends.contains(curr) || curr < (Test.alpha(0) ^ 32) || curr > (Test.alpha(numOfUsers) ^ 32))
-                continue;
-            friends.add(curr);
-
-            int LF = curr - 2; // Left Friend
-            if (LF >= (Test.alpha(0) ^ 32)) {
-                createFriendshipReturningRequestID(
-                        (char) MAIN_USER,
-                        queue,
-                        (char) curr,
-                        (char) LF
-                );
-            }
-
-            int RF = curr + 2;  // Right Friend
-            if (RF < (Test.alpha(numOfUsers) ^ 32)) {
-                createFriendshipReturningRequestID(
-                        (char) MAIN_USER,
-                        queue,
-                        (char) curr,
-                        (char) RF
-                );
-            }
-        }
-
-
-        var numOfTotalRequests = friendshipRequestService.findNumOfTotalRequests();
-        for (int i = 0; i < numOfTotalRequests;) {
-            acceptFriendRequest(++i);
-        }
-
-        System.out.println("DONE");
-
-        return email.formatted(MAIN_USER);
+        return Test.createFriends(numOfUsers, friendshipRequestService, this::acceptFriendRequest, this::sendFriendRequest);
     }
 
-    private void createFriendshipReturningRequestID(char MAIN_USER,
-                                                    Queue<Integer> queue,
-                                                    char curr,
-                                                    char friendNum) {
-        sendFriendRequest(
-                "%c@dal.ca".formatted(curr),
-                "%c@dal.ca".formatted(friendNum)
-        );
 
-        if (MAIN_USER != friendNum) {
-            sendFriendRequest(
-                    "%c@dal.ca".formatted(MAIN_USER),
-                    "%c@dal.ca".formatted(friendNum)
-            );
-        }
-
-        queue.add((int) friendNum);
-    }
 }
--- src/main/java/com/group2/Tiger_Talks/backend/model/Test.java
+++ src/main/java/com/group2/Tiger_Talks/backend/model/Test.java
@@ -1,9 +1,16 @@
 package com.group2.Tiger_Talks.backend.model;
 
+import com.group2.Tiger_Talks.backend.model.User.UserProfile;
 import com.group2.Tiger_Talks.backend.model.Utils.UserStatus;
+import com.group2.Tiger_Talks.backend.service.FriendshipRequestService;
+import org.springframework.http.ResponseEntity;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import static com.group2.Tiger_Talks.backend.model.Utils.TestGen.MAX_USERS;
+import static com.group2.Tiger_Talks.backend.model.Utils.TestGen.MIN_USERS;
 
 public final class Test {
 
@@ -40,5 +47,78 @@ public final class Test {
 
     public static char alpha(int n) {
         return (char) ('A' + n);
+    }
+
+    public static String createFriends(int numOfUsers,
+                                       FriendshipRequestService friendshipRequestService,
+                                       Function<Integer, ResponseEntity<String>> acceptFriendRequest,
+                                       BiFunction<String, String, ResponseEntity<String>> sendFriendRequest) {
+        if (numOfUsers > MAX_USERS || MIN_USERS > numOfUsers)
+            throw new IllegalArgumentException("Cannot generate more than 26 and less than 3 users as the names are A-Z check impl to change this behaviour");
+
+        int MAIN_USER = Test.alpha((numOfUsers / 2)) ^ 32;
+        Queue<Integer> queue = new LinkedList<>(List.of(MAIN_USER));
+        HashSet<Integer> friends = new HashSet<>();
+
+        final String email = "%c@dal.ca";
+
+        while (!queue.isEmpty()) {
+            int curr = queue.remove();
+            if (friends.contains(curr) || curr < (Test.alpha(0) ^ 32) || curr > (Test.alpha(numOfUsers) ^ 32))
+                continue;
+            friends.add(curr);
+
+            int LF = curr - 2; // Left Friend
+            if (LF >= (Test.alpha(0) ^ 32)) {
+                createFriendshipReturningRequestID(
+                        (char) MAIN_USER,
+                        queue,
+                        (char) curr,
+                        (char) LF,
+                        sendFriendRequest
+                );
+            }
+
+            int RF = curr + 2;  // Right Friend
+            if (RF < (Test.alpha(numOfUsers) ^ 32)) {
+                createFriendshipReturningRequestID(
+                        (char) MAIN_USER,
+                        queue,
+                        (char) curr,
+                        (char) RF,
+                        sendFriendRequest
+                );
+            }
+        }
+
+
+        var numOfTotalRequests = friendshipRequestService.findNumOfTotalRequests();
+        for (int i = 0; i < numOfTotalRequests; ) {
+            acceptFriendRequest.apply(++i);
+        }
+
+        System.out.println("DONE");
+
+        return email.formatted(MAIN_USER);
+    }
+
+    private static void createFriendshipReturningRequestID(char MAIN_USER,
+                                                           Queue<Integer> queue,
+                                                           char curr,
+                                                           char friendNum,
+                                                           BiFunction<String, String, ResponseEntity<String>> sendFriendRequest) {
+        sendFriendRequest.apply(
+                "%c@dal.ca".formatted(curr),
+                "%c@dal.ca".formatted(friendNum)
+        );
+
+        if (MAIN_USER != friendNum) {
+            sendFriendRequest.apply(
+                    "%c@dal.ca".formatted(MAIN_USER),
+                    "%c@dal.ca".formatted(friendNum)
+            );
+        }
+
+        queue.add((int) friendNum);
     }
 }
