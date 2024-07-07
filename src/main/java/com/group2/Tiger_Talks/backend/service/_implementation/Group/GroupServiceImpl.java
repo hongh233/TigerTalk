98291d326d4895351a859d5a57b1a819200a2a75
hongh931 <hn582183@dal.ca>
2024-07-07 16:51:33 -0300

    feat!:add implementation for the Group Api

---
+++ src/main/java/com/group2/Tiger_Talks/backend/service/_implementation/Group/GroupServiceImpl.java
@@ -0,0 +1,123 @@
+package com.group2.Tiger_Talks.backend.service._implementation.Group;
+
+import com.group2.Tiger_Talks.backend.model.Group.*;
+import com.group2.Tiger_Talks.backend.model.User.UserProfile;
+import com.group2.Tiger_Talks.backend.repository.Group.GroupMembershipRepository;
+import com.group2.Tiger_Talks.backend.repository.Group.GroupRepository;
+import com.group2.Tiger_Talks.backend.repository.User.UserProfileRepository;
+import com.group2.Tiger_Talks.backend.service.Group.GroupService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class GroupServiceImpl implements GroupService {
+
+    @Autowired
+    private GroupRepository groupRepository;
+
+    @Autowired
+    private GroupMembershipRepository groupMembershipRepository;
+
+    @Autowired
+    private UserProfileRepository userProfileRepository;
+
+    @Override
+    public Optional<String> createGroup(String groupName, String creatorEmail, boolean isPrivate) {
+        if (userProfileRepository.findById(creatorEmail).isEmpty()) {
+            return Optional.of("User not found");
+        }
+        UserProfile userProfile = userProfileRepository.findById(creatorEmail).get();
+
+        Group group = new Group(groupName, isPrivate);
+        groupRepository.save(group);
+        GroupMembership groupMembership = new GroupMembership(group, userProfile, true);
+        groupMembershipRepository.save(groupMembership);
+        return Optional.of("Group successfully created");
+    }
+
+    public Optional<String> joinGroup(String userEmail, int groupId) {
+        if (userProfileRepository.findById(userEmail).isEmpty()) {
+            return Optional.of("User not found");
+        }
+        if (groupRepository.findById(groupId).isEmpty()) {
+            return Optional.of("Group not found");
+        }
+
+        UserProfile userProfile = userProfileRepository.findById(userEmail).get();
+        Group group = groupRepository.findById(groupId).get();
+
+        // Check if the user is already a member of the group
+        for (GroupMembership membership : group.getGroupMemberList()) {
+            if (membership.getUserProfile().equals(userProfile)) {
+                return Optional.of("User is already a member of the group");
+            }
+        }
+        GroupMembership groupMembership = new GroupMembership(group, userProfile, false);
+        groupMembershipRepository.save(groupMembership);
+
+        return Optional.of("User successfully joined the group");
+    }
+
+    @Override
+    public List<GroupDTO> getAllGroups() {
+        return groupRepository.findAll().stream()
+                .map(GroupDTO::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public GroupDTO getGroup(int groupId) {
+        Optional<Group> group = groupRepository.findById(groupId);
+        return group.map(GroupDTO::new).orElse(null);
+    }
+
+    @Override
+    public Optional<String> updateGroupInfo(GroupUpdate groupUpdate) {
+        Optional<Group> groupTemp = groupRepository.findById(groupUpdate.getGroupId());
+        if (groupTemp.isEmpty()) {
+            return Optional.of("Group id not found");
+        }
+        Group group = groupTemp.get();
+        group.setGroupName(groupUpdate.getGroupName());
+        group.setGroupImg(groupUpdate.getGroupImg());
+        group.setPrivate(groupUpdate.isPrivate());
+        groupRepository.save(group);
+        return Optional.of("Group Info Successfully updated");
+    }
+
+    @Override
+    public Optional<String> deleteGroup(int groupId) {
+        Optional<Group> groupTemp = groupRepository.findById(groupId);
+        if (groupTemp.isEmpty()) {
+            return Optional.of("Group id not found");
+        }
+        Group group = groupTemp.get();
+        groupMembershipRepository.deleteAll(group.getGroupMemberList());
+        groupRepository.delete(group);
+        return Optional.of("Group Successfully deleted");
+    }
+
+    // I haven't considered whether we can delete group creator
+    @Override
+    public Optional<String> deleteGroupMembership(int groupMembershipId) {
+        Optional<GroupMembership> membershipTemp = groupMembershipRepository.findById(groupMembershipId);
+        if (membershipTemp.isEmpty()) {
+            return Optional.of("Group membership id not found");
+        }
+        groupMembershipRepository.delete(membershipTemp.get());
+        return Optional.of("Group membership Successfully deleted");
+    }
+
+    @Override
+    public List<GroupMembershipDTO> getGroupMembersByGroupId(int groupId) {
+        List<GroupMembership> memberships = groupMembershipRepository.findByGroup_GroupId(groupId);
+        return memberships.stream()
+                .map(GroupMembershipDTO::new)
+                .collect(Collectors.toList());
+    }
+
+}
