bb8686958cb6048e34bfc997f4ba500c93c17113
hongh931 <hn582183@dal.ca>
2024-07-07 16:50:32 -0300

    feat: update the CRUD method for group repository

---
+++ src/main/java/com/group2/Tiger_Talks/backend/repository/Group/GroupMessageRepository.java
@@ -0,0 +1,7 @@
+package com.group2.Tiger_Talks.backend.repository.Group;
+
+import com.group2.Tiger_Talks.backend.model.Group.GroupPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupMessageRepository extends JpaRepository<GroupPost, Integer> {
+}
