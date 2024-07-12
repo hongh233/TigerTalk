0bf5cac5b60d3ae95936d3425d764d59d359cf99
hongh931 <hn582183@dal.ca>
2024-07-12 17:15:44 -0300

    feat: add new CRUD for Group Repository

--- src/main/java/com/group2/Tiger_Talks/backend/repository/Group/GroupMembershipRepository.java
+++ src/main/java/com/group2/Tiger_Talks/backend/repository/Group/GroupMembershipRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface GroupMembershipRepository extends JpaRepository<GroupMembership, Integer> {
     List<GroupMembership> findByGroup_GroupId(int groupId);
+    List<GroupMembership> findByUserProfile_Email(String email);
 }
