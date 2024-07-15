f37f7e4ad8968a575c70562418e9742615200cc6
hongh931 <hn582183@dal.ca>
2024-07-15 13:30:01 -0300

    feat: add new models to Group include group post and group comments

---
+++ src/main/java/com/group2/Tiger_Talks/backend/model/Group/GroupPostCommentDTO.java
@@ -0,0 +1,42 @@
+package com.group2.Tiger_Talks.backend.model.Group;
+
+import java.time.LocalDateTime;
+
+public class GroupPostCommentDTO {
+
+    private final int groupPostCommentId;
+    private final String groupPostCommentContent;
+    private final LocalDateTime groupPostCommentCreateTime;
+    private final String groupPostCommentSenderUserName;
+    private final String groupPostCommentSenderProfilePictureURL;
+
+
+    public GroupPostCommentDTO(GroupPostComment groupPostComment) {
+        this.groupPostCommentId = groupPostComment.getGroupPostCommentId();
+        this.groupPostCommentContent = groupPostComment.getContent();
+        this.groupPostCommentCreateTime = groupPostComment.getGroupPostCommentCreateTime();
+        this.groupPostCommentSenderUserName = groupPostComment.getGroupMembership().getUserProfile().getUserName();
+        this.groupPostCommentSenderProfilePictureURL = groupPostComment.getGroupMembership().getUserProfile().getProfilePictureUrl();
+    }
+
+    public int getGroupPostCommentId() {
+        return groupPostCommentId;
+    }
+
+    public String getGroupPostCommentContent() {
+        return groupPostCommentContent;
+    }
+
+    public LocalDateTime getGroupPostCommentCreateTime() {
+        return groupPostCommentCreateTime;
+    }
+
+    public String getGroupPostCommentSenderUserName() {
+        return groupPostCommentSenderUserName;
+    }
+
+    public String getGroupPostCommentSenderProfilePictureURL() {
+        return groupPostCommentSenderProfilePictureURL;
+    }
+
+}
---
+++ src/main/java/com/group2/Tiger_Talks/backend/model/Group/GroupPostDTO.java
@@ -0,0 +1,65 @@
+package com.group2.Tiger_Talks.backend.model.Group;
+
+import com.group2.Tiger_Talks.backend.model.User.UserProfile;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+
+public class GroupPostDTO {
+
+    private final int groupPostId;
+    private final String postSenderEmail;
+    private final String groupPostContent;
+    private final LocalDateTime groupPostCreateTime;
+    private final String groupPostSenderUserName;
+    private final String groupPostSenderProfilePictureURL;
+
+
+    public GroupPostDTO(GroupPost groupPost) {
+        this.groupPostId = groupPost.getGroupPostId();
+        this.postSenderEmail = groupPost.getGroupPostSenderEmail();
+        this.groupPostContent = groupPost.getGroupPostContent();
+        this.groupPostCreateTime = groupPost.getPostCreateTime();
+        this.groupPostSenderUserName = Objects.requireNonNull(findUserProfileByEmail(groupPost)).getUserName();
+        this.groupPostSenderProfilePictureURL = Objects.requireNonNull(findUserProfileByEmail(groupPost)).getProfilePictureUrl();
+
+    }
+
+    private UserProfile findUserProfileByEmail(GroupPost groupPost){
+        String email = groupPost.getGroupPostSenderEmail();
+        List<GroupMembership> groupMembershipList = groupPost.getGroup().getGroupMemberList();
+        for (GroupMembership groupMembership : groupMembershipList) {
+            UserProfile userProfile = groupMembership.getUserProfile();
+            if(userProfile.getEmail().equals(email)){
+               return userProfile;
+            }
+        }
+        return null;
+    }
+
+    public int getGroupPostId() {
+        return groupPostId;
+    }
+
+    public String getPostSenderEmail() {
+        return postSenderEmail;
+    }
+
+    public String getGroupPostContent() {
+        return groupPostContent;
+    }
+
+    public LocalDateTime getGroupPostCreateTime() {
+        return groupPostCreateTime;
+    }
+
+    public String getGroupPostSenderUserName() {
+        return groupPostSenderUserName;
+    }
+
+    public String getGroupPostSenderProfilePictureURL() {
+        return groupPostSenderProfilePictureURL;
+    }
+
+}
