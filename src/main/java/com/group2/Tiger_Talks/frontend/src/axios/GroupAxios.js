85f4976a35580b5f0070c6fe7ed898526c6b9875
Thai Son Pham <th811292@dal.ca>
2024-07-16 11:20:10 -0300

    adding UI for group logic, leave group logic is not working

--- src/main/java/com/group2/Tiger_Talks/frontend/src/axios/GroupAxios.js
+++ src/main/java/com/group2/Tiger_Talks/frontend/src/axios/GroupAxios.js
@@ -14,12 +14,51 @@ export const handleCreateGroup = (form) => {
 		});
 };
 
+export const handleGetGroupById = (groupId) => {
+	return axios
+		.get(`${URL}/api/groups/get/group/${groupId}`)
+		.then((response) => response.data)
+		.catch((error) => {
+			console.error("Error getting all groups");
+			throw error;
+		});
+};
+
+export const checkIsMember = (userEmail, groupId) => {
+	return axios
+		.get(`${URL}/api/groups/get/isMember/${userEmail}/${groupId}`)
+		.then((response) => response.data)
+		.catch((error) => {
+			console.error("Error getting all groups");
+			throw error;
+		});
+};
 export const handleGetAllGroups = () => {
 	return axios
 		.get(`${URL}/api/groups/get/allGroups`)
 		.then((response) => response.data)
 		.catch((error) => {
 			console.error("Error getting all groups");
+			throw error;
+		});
+};
+
+export const handleJoinGroup = (userEmail, groupId) => {
+	return axios
+		.post(`${URL}/api/groups/join/${userEmail}/${groupId}`)
+		.then((response) => response.data)
+		.catch((error) => {
+			console.error("Error joining user to the group");
+			throw error;
+		});
+};
+
+export const handleLeaveGroup = (groupMembershipId) => {
+	return axios
+		.delete(`${URL}/api/groups/delete/groupMembership/${groupMembershipId}`)
+		.then((response) => response.data)
+		.catch((error) => {
+			console.error("Error joining user to the group");
 			throw error;
 		});
 };
