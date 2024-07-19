5780a4316d10a5d745e6579d61d6f2662326e8bd
hongh931 <hn582183@dal.ca>
2024-07-19 17:42:35 -0300

    feat: fix post notification bugs, use friendshipRepository getALlFriendsByEmail instead of getAllFriends in UserProfile

--- src/main/java/com/group2/Tiger_Talks/backend/service/_implementation/Friend/FriendshipRecommendationServiceImpl.java
+++ src/main/java/com/group2/Tiger_Talks/backend/service/_implementation/Friend/FriendshipRecommendationServiceImpl.java
@@ -2,6 +2,7 @@ package com.group2.Tiger_Talks.backend.service._implementation.Friend;
 
 import com.group2.Tiger_Talks.backend.model.User.UserProfile;
 import com.group2.Tiger_Talks.backend.model.User.UserProfileDTOPost;
+import com.group2.Tiger_Talks.backend.repository.Socials.FriendshipRepository;
 import com.group2.Tiger_Talks.backend.repository.User.UserProfileRepository;
 import com.group2.Tiger_Talks.backend.service.Friend.FriendshipRecommendationService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +19,15 @@ public class FriendshipRecommendationServiceImpl implements FriendshipRecommenda
     @Autowired
     private UserProfileRepository userProfileRepository;
 
+    @Autowired
+    private FriendshipRepository friendshipRepository;
+
     public List<UserProfileDTOPost> recommendFriends(String email, int numOfFriends) {
         // Retrieve the user's profile and their list of friends
         return userProfileRepository.findById(email)
                 .map(myProfile -> {
                     // Assumes getAllFriends() returns a List<UserProfile>
-                    List<UserProfile> allMyFriends = myProfile.getAllFriends();
+                    List<UserProfile> allMyFriends = friendshipRepository.findAllFriendsByEmail(myProfile.getEmail());
 
                     // Get all potential friends, except for current friends and the user themselves
                     List<UserProfile> potentialFriends = userProfileRepository.findAll().stream()
