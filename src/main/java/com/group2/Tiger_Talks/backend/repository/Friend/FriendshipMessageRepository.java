fda5766b351c75d8fa657612c5b7c05eb0fb3fdb
hongh931 <hn582183@dal.ca>
2024-07-21 18:02:41 -0300

    feat: add new select to FriendshipMessageRepository

--- src/main/java/com/group2/Tiger_Talks/backend/repository/Friend/FriendshipMessageRepository.java
+++ src/main/java/com/group2/Tiger_Talks/backend/repository/Friend/FriendshipMessageRepository.java
@@ -11,5 +11,5 @@ import java.util.List;
 import java.util.Optional;
 
 public interface FriendshipMessageRepository extends JpaRepository<FriendshipMessage, Integer> {
-
+    List<FriendshipMessage> findByFriendship_FriendshipId(int friendshipId);
 }
