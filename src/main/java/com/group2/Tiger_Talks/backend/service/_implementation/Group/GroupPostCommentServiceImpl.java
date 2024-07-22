77133102ac757ae36951c4e2d58084af1d322b76
hongh931 <hn582183@dal.ca>
2024-07-22 01:04:48 -0300

    feat: add implementation for create group post comment notification

--- src/main/java/com/group2/Tiger_Talks/backend/service/_implementation/Group/GroupPostCommentServiceImpl.java
+++ src/main/java/com/group2/Tiger_Talks/backend/service/_implementation/Group/GroupPostCommentServiceImpl.java
@@ -4,10 +4,14 @@ import com.group2.Tiger_Talks.backend.model.Group.GroupMembership;
 import com.group2.Tiger_Talks.backend.model.Group.GroupPost;
 import com.group2.Tiger_Talks.backend.model.Group.GroupPostComment;
 import com.group2.Tiger_Talks.backend.model.Group.GroupPostCommentDTO;
+import com.group2.Tiger_Talks.backend.model.Notification.Notification;
+import com.group2.Tiger_Talks.backend.model.User.UserProfile;
 import com.group2.Tiger_Talks.backend.repository.Group.GroupMembershipRepository;
 import com.group2.Tiger_Talks.backend.repository.Group.GroupPostCommentRepository;
 import com.group2.Tiger_Talks.backend.repository.Group.GroupPostRepository;
+import com.group2.Tiger_Talks.backend.repository.User.UserProfileRepository;
 import com.group2.Tiger_Talks.backend.service.Group.GroupPostCommentService;
+import com.group2.Tiger_Talks.backend.service.Notification.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -29,6 +33,12 @@ public class GroupPostCommentServiceImpl implements GroupPostCommentService {
     @Autowired
     private GroupPostCommentRepository groupPostCommentRepository;
 
+    @Autowired
+    private UserProfileRepository userProfileRepository;
+
+    @Autowired
+    private NotificationService notificationService;
+
     @Override
     public Optional<String> createGroupPostComment(int groupPostId, GroupPostComment groupPostComment) {
         Optional<GroupPost> groupPostOpt = groupPostRepository.findById(groupPostId);
@@ -41,6 +51,24 @@ public class GroupPostCommentServiceImpl implements GroupPostCommentService {
             if (groupMembershipOpt.isPresent()) {
                 groupPostComment.setGroupMembership(groupMembershipOpt.get());
                 groupPostCommentRepository.save(groupPostComment);
+
+                // Create and send notification to GroupPost owner if it's not the same as the commenter
+                String groupPostOwnerEmail = groupPost.getGroupPostSenderEmail();
+                String commenterEmail = groupPostComment.getGroupMembership().getUserProfile().getEmail();
+                if (!groupPostOwnerEmail.equals(commenterEmail)) {
+                    Optional<UserProfile> groupPostOwnerOpt = userProfileRepository.findById(groupPostOwnerEmail);
+                    if (groupPostOwnerOpt.isPresent()) {
+                        UserProfile groupPostOwner = groupPostOwnerOpt.get();
+                        Notification notification = new Notification(
+                                groupPostOwner,
+                                "User " + commenterEmail +
+                                        " commented on your post in group " + groupPost.getGroup().getGroupName(),
+                                "GroupPostComment"
+                        );
+                        notificationService.createNotification(notification);
+                    }
+                }
+
                 return Optional.empty();
             } else {
                 return Optional.of("User is not a member of the group, fail to create group post comment.");
