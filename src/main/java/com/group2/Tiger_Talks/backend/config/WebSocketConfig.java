097e337ca90d787682c5b4f488733a17fac0f5e8
hongh931 <hn582183@dal.ca>
2024-07-23 18:56:04 -0300

    feat: add socekt io controller to the backend

--- src/main/java/com/group2/Tiger_Talks/backend/config/WebSocketConfig.java
+++ src/main/java/com/group2/Tiger_Talks/backend/config/WebSocketConfig.java
@@ -18,6 +18,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("http://localhost:3000")
+                .withSockJS();
     }
 }
