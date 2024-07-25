246dff486f8a088b0311aa2c4cd47155f79f1bf8
hongh931 <hn582183@dal.ca>
2024-07-25 15:28:56 -0300

    refactor: move friendMessagePage axio to FriendAxios

--- src/main/java/com/group2/Tiger_Talks/frontend/src/axios/FriendAxios.js
+++ src/main/java/com/group2/Tiger_Talks/frontend/src/axios/FriendAxios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+
 const URL = process.env.REACT_APP_API_URL;
 
-// This axio is used in FriendListPage
+// This axio is used in FriendListPage, FriendMessagePage
 export const getAllFriendsByEmail = async (email) => {
     try {
         const response = await axios.get(`${URL}/friendships/DTO/${email}`);
@@ -24,5 +25,23 @@ export const handleDelete = async (senderEmail, receiverEmail) => {
     } catch (error) {
         window.alert("Failed to delete friend. Please try again.");
         console.error(error);
+    }
+};
+
+// This axio is used in FriendMessagePage
+export const getAllMessagesByFriendshipId = async (friendshipId) => {
+    try {
+        const response = await axios.get(`${URL}/friendships/message/getAll/${friendshipId}`);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const createMessage = async (message) => {
+    try {
+        return await axios.post(`${URL}/friendships/message/create`, message);
+    } catch (error) {
+        throw error;
     }
 };
