12b2dd80151ff3b8b94dcb2a9e0d036c25a1dcc8
hongh931 <hn582183@dal.ca>
2024-07-25 12:46:46 -0300

    refactor: move axio calling in signUp to a common AxioFile

--- src/main/java/com/group2/Tiger_Talks/frontend/src/axios/AuthenticationAxios.js
+++ src/main/java/com/group2/Tiger_Talks/frontend/src/axios/AuthenticationAxios.js
@@ -26,3 +26,30 @@ export const userLogin = async (email, password) => {
 		throw error;
 	}
 };
+
+export const userSignUp = async (form) => {
+	try {
+		const response = await axios.post(`${URL}/api/signUp/userSignUp`, {
+			firstName: form.firstName,
+			lastName: form.lastName,
+			age: form.age,
+			gender: form.gender,
+			userName: form.userName,
+			email: form.email,
+			password: form.password,
+			securityQuestionsAnswer: [
+				form.securityAnswer1,
+				form.securityAnswer2,
+				form.securityAnswer3,
+			],
+			securityQuestions: [
+				form.securityQuestion1,
+				form.securityQuestion2,
+				form.securityQuestion3,
+			],
+		});
+		return response.data;
+	} catch (error) {
+		throw error;
+	}
+};
--- src/main/java/com/group2/Tiger_Talks/frontend/src/pages/Authentication/LoginPage.js
+++ src/main/java/com/group2/Tiger_Talks/frontend/src/pages/Authentication/LoginPage.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
 import "../../assets/styles/Authentication/LoginPage.css";
 
 import { useDispatch } from "react-redux";
