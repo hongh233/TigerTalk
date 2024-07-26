7745d93ce73d2e293710dc27483876eb62929db2
hongh931 <hn582183@dal.ca>
2024-07-26 20:02:28 -0300

    docs: add comment to UserSearchController

--- src/main/java/com/group2/Tiger_Talks/backend/controller/Search/UserSearchController.java
+++ src/main/java/com/group2/Tiger_Talks/backend/controller/Search/UserSearchController.java
@@ -17,6 +17,13 @@ public class UserSearchController {
     @Autowired
     private Searchable<UserProfileDTO> userSearchService;
 
+    /**
+     * Searches for users based on the search query and user email.
+     *
+     * @param searchQuery the search query string
+     * @param userEmail   the email of the user
+     * @return a list of UserProfileDTO objects matching the search criteria
+     */
     @GetMapping("/{searchQuery}/{userEmail}")
     public List<UserProfileDTO> searchUsers(@PathVariable String searchQuery, @PathVariable String userEmail) {
         return userSearchService.search(searchQuery, userEmail);
