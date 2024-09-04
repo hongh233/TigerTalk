93049cc8542e691fb6b772d8b577d96a7e93629a
hongh931 <hn582183@dal.ca>
2024-09-04 04:28:27 -0300

    refactor: move same security question list onto same file

--- frontend/src/Pages/Authentication/ForgotPasswordPage.js
+++ frontend/src/Pages/Authentication/ForgotPasswordPage.js
@@ -8,25 +8,7 @@ import {
 } from "../../axios/Authentication/PasswordResetAxios";
 import { useNavigate } from 'react-router-dom';
 import {updateUserPassword} from "../../axios/UserAxios";
-
-
-const SECURITY_QUESTIONS = [
-    "What was your favourite book as a child?",
-    "What school did you attend for grade nine?",
-    "What was the last name of your grade three teacher?",
-    "What is your favourite person from history?",
-    "What was the first video game you have ever played?",
-    "In what city were you born?",
-    "What was the name of your elementary school?",
-    "What was the name of your best friend in high school?",
-    "What was your childhood nickname?",
-    "Where did you go on your first vacation?",
-    "What is the name of the hospital where you were born?",
-    "What is your favourite food?",
-    "What is the name of your favorite childhood toy?",
-    "What is your favorite movie?",
-    "What is your favorite restaurant?",
-];
+import { SECURITY_QUESTIONS } from "../../utils/securityQuestions";
 
 
 const ForgotPasswordPage = () => {
---
+++ frontend/src/utils/securityQuestions.js
@@ -0,0 +1,17 @@
+export const SECURITY_QUESTIONS = [
+    "What was your favourite book as a child?",
+    "What school did you attend for grade nine?",
+    "What was the last name of your grade three teacher?",
+    "What is your favourite person from history?",
+    "What was the first video game you have ever played?",
+    "In what city were you born?",
+    "What was the name of your elementary school?",
+    "What was the name of your best friend in high school?",
+    "What was your childhood nickname?",
+    "Where did you go on your first vacation?",
+    "What is the name of the hospital where you were born?",
+    "What is your favourite food?",
+    "What is the name of your favorite childhood toy?",
+    "What is your favorite movie?",
+    "What is your favorite restaurant?",
+];
