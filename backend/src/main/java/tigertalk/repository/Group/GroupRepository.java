aa6367fce4088d7b59e4ebd1b3ec082b741c4737
hongh931 <hn582183@dal.ca>
2024-09-06 06:43:01 -0300

    feat: add group search logic for backend

--- backend/src/main/java/tigertalk/repository/Group/GroupRepository.java
+++ backend/src/main/java/tigertalk/repository/Group/GroupRepository.java
@@ -1,8 +1,21 @@
 package tigertalk.repository.Group;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import tigertalk.model.Group.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GroupRepository extends JpaRepository<Group, Integer> {
 
+    // We don't care about uppercase or lowercase
+    @Query("SELECT g FROM Group g " +
+            "WHERE LOWER(g.groupName) LIKE LOWER(CONCAT('%', :groupName, '%')) " +
+            "ORDER BY CASE " +
+            "WHEN LOWER(g.groupName) = LOWER(:groupName) THEN 1 " +
+            "WHEN LOWER(g.groupName) LIKE LOWER(CONCAT(:groupName, '%')) THEN 2 " +
+            "WHEN LOWER(g.groupName) LIKE LOWER(CONCAT('%', :groupName)) THEN 3 " +
+            "ELSE 4 END, g.groupName ASC")
+    List<Group> searchByGroupName(@Param("groupName") String groupName);
 }
