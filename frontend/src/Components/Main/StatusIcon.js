69f81b4a7984548d0b849feb2ec542cb0f65d69e
hongh931 <hn582183@dal.ca>
2024-09-06 06:31:24 -0300

    feat: add common status icon components for all profile picture

---
+++ frontend/src/Components/Main/StatusIcon.js
@@ -0,0 +1,35 @@
+import {MdAccessTimeFilled, MdCheckCircle, MdRemoveCircle} from "react-icons/md";
+import "../../assets/styles/Components/Main/StatusIcon.css";
+import {IoMdCloseCircle} from "react-icons/io";
+import React from "react";
+
+
+/* How to use: set direct parent box to be:
+    position: relative;
+*/
+const getStatusClass = (status) => {
+    switch (status) {
+        case "available":
+            return <MdCheckCircle
+                style={{ color: '#4caf50' }}
+                className="status-adjust-creation-small"
+            />;
+        case "busy":
+            return <MdRemoveCircle
+                style={{ color: '#f44336' }}
+                className="status-adjust-creation-small"
+            />;
+        case "away":
+            return <MdAccessTimeFilled
+                style={{ color: '#ff9800' }}
+                className="status-adjust-creation-small"
+            />;
+        default:
+            return <IoMdCloseCircle
+                style={{ color: '#9e9e9e' }}
+                className="status-adjust-creation-small"
+            />;
+    }
+};
+
+export default getStatusClass;
