90284c5571159e92b98e410450690da35115353d
hongh931 <hn582183@dal.ca>
2024-09-08 01:46:03 -0300

    feat: add online status onto message DTO

--- backend/src/main/java/tigertalk/model/Friend/FriendshipMessageDTO.java
+++ backend/src/main/java/tigertalk/model/Friend/FriendshipMessageDTO.java
@@ -7,12 +7,17 @@ public record FriendshipMessageDTO(
         int messageId,
         LocalDateTime createTime,
         String messageContent,
+
         String messageSenderEmail,
         String messageSenderUserName,
         String messageSenderProfilePictureUrl,
+        String messageSenderOnlineStatus,
+
         String messageReceiverEmail,
         String messageReceiverUserName,
         String messageReceiverProfilePictureUrl,
+        String messageReceiverOnlineStatus,
+
         boolean isRead,
         int friendshipId
 ) {
