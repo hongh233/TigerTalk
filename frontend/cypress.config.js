bf632c3b1c0e29f522a73ecf175fd97da610f01d
hongh931 <hn582183@dal.ca>
2024-09-20 04:26:48 -0300

    add configuration for cypress

---
+++ frontend/cypress.config.js
@@ -0,0 +1,11 @@
+// This section is required for using cypress!!!
+
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
